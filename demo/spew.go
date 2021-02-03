84f284a14516e5e9aa3e36de13eea325f13c6300
devhg <501966782@qq.com>
2021-02-03 22:21:36 +0800

    a & u

---
+++ demo/spew.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+	"reflect"
+	"unsafe"
+)
+
+type Person struct {
+	Name string
+}
+
+type Foo struct {
+	p Person
+}
+
+func main() {
+	s0 := "something"
+	s1 := "something"
+	s2 := "something"[3:]
+	spew.Dump(s0, s1, s2)
+
+	spew.Dump(&s0)
+	spew.Dump(&s1)
+	spew.Dump((*reflect.StringHeader)(unsafe.Pointer(&s0)))
+	spew.Dump((*reflect.StringHeader)(unsafe.Pointer(&s1)))
+
+	f := Foo{p: Person{Name: "123"}}
+	mp := map[int]int{1: 23}
+	b := []byte{
+		0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+		0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20,
+		0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28,
+		0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30,
+		0x31, 0x32,
+	}
+	spew.Dump(f, mp, b, s0)
+}
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	_ "net/http/pprof"
+	"time"
+)
 
 /*-------------------INPUT-------------------*/
 //var reader = bufio.new(os.Stdin)
@@ -10,9 +15,43 @@ import "fmt"
 //var reader = bufio.NewScanner(os.Stdin)
 
 func main() {
-	mp := map[int]interface{}{}
-	mp[1] = 1
-	if _, ok := mp[1]; ok {
-		fmt.Println(ok)
+	parse, err := time.Parse("2006-01-02 15:04:05", "2021-02-03 21:12:40")
+	if err != nil {
+		panic(err)
+	}
+	ctx, _ := context.WithDeadline(context.Background(), parse)
+	ok := make(chan int)
+	begin := make(chan int)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done(): // 定时开始
+				fmt.Println(123)
+				ok <- 1
+			case <-begin: // 条件开始
+			}
+		}
+	}()
+	<-ok
+	ctx1, cancel := context.WithCancel(context.Background())
+	go func() {
+		for {
+			select {
+			case <-time.After(2 * time.Second): // 定时退出
+				cancel()
+			case <-ctx1.Done(): // 条件对出
+				cancel()
+			default:
+				time.Sleep(time.Second)
+				fmt.Println("heat")
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		}
 	}
 }
